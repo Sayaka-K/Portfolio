709cf298d358b0c19b7c1f349a9d04f2ac609926
skuwayam <skuwayam@ucsd.edu>
2021-09-26 23:09:38 -0700

    updated contents

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,30 +14,30 @@ function Cards(){
                 <ul className='cards__items'>
                     <CardItem 
                     src='images/pricing.png'
-                    title='Kellogg Pricing Analytics'
-                    text='This Android Application incorporates CRUD functionallity on a Vehicle Management System.
-                    I utilized the M.V.C. desing patern, Java, XML, and Firebase Webservices to bulid the application
-                    and Espresso to conduct testing.'
-                    label='Project'
+                    title="Kellogg's Pricing Analytics"
+                    text="Leveraged data to suggest 'more promising' prices for Kellogg's
+                    cereals that would not only solidify Kellogg's market dominance but also lead to increased profits. 
+                    The project includes Visualizations, Optimal Pricing Setting, Competition Analysis, and Predictive Modeling."
+                    label='Pricing analytics'
                     path='https://github.com/Sayaka-K/Kellogg-s-Pricing-Analytics/blob/main/presentation.pdf' />
 
                     <CardItem 
                     src='images/shiny.png'
-                    title='Shiny Application'
-                    text="I am a Teaching Assistant at the Computer Science Department
-                    at SDSU. I write Java utilities that act as tools for students to 
-                    use in their programs. I also write programs that test and validate the 
-                    student's programming assignments. I hold office hours where I can help improve
-                    students understanding of Data Structures and Java."
+                    title='Shiny Dashboards'
+                    text="As a Data Analyst at the SOL-INCA in the Department of Neurosciences at UC San Diego, 
+                    I investigate associations between neurocognitive aging and other health conditions. 
+                    The analysis using publicly available data is to be added soon!
+                    For now, feel free to see the sample R Markdown document I created with Shiny interactive features."
                     label='Work'
                     path='http://sayaka-app.shinyapps.io/Testing' />
 
                     <CardItem 
                     src='images/petco-logo.png'
-                    title='Data Science competition - Winner'
-                    text="I'm a project lead at the SDSU Artificial Intelligence Club.
-                    I led a project where we used Data Maniupulation and Machine Leaning models to 
-                    built income prediction models."
+                    title='Case competition - Winner'
+                    text="Participated in a school-wide data science competition hosted by Petco and 
+                    Rady Data Analytics Club at UC San Diego. 
+                    The challenge was to construct more granular and accurate revenue forecasting models to complement Petco’s current models.
+                    I cannot go into specific details since this project is under NDA; however, please check out the flyer here."
                     label='Datathon'
                     path='https://www.linkedin.com/posts/rady-data-analytics-club_petco-casestudy-casestudycompetition-activity-6791217381898289152-AKiv' />
 
@@ -46,34 +46,31 @@ function Cards(){
 
 
                     <CardItem 
-                    src='images/fraud1.PNG'
-                    title='Property Fraud'
-                    text="This React application uses the Numbers API to 
-                    generate random facts about numbers. I created this 
-                    application to practice consuming API's with React.js."
-                    label='Project'
+                    src='images/fraud1.png'
+                    title='New York Property Fraud'
+                    text="Built unsurpervised models (Z-Score outliers and Autoencoder) to detect if any of the properties records from City of New York
+                    were out of the ordinary and determined potential candidates for further fraud investigation. 
+                    Examples of techniques used are Variable creations and Dimensionality reduction(PCA.)"
+                    label='Unsupervised learning'
                     path='https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:147e4a70-8acb-4ce0-87f4-4d85d46b9902' />
 
                     <CardItem 
                     src='images/fraud2.png'
-                    title='Credit Card Transaction Fraud'
-                    text='A brainchild of mine, this 
-                    is an Android parking application 
-                    for university students and faculty 
-                    to find available parking spaces. 
-                    I used Java, XML, Firebase Webservices and the Google Maps API.'
-                    label='Project'
+                    title='Credit Card Fraud'
+                    text='Built supervised machine learning algorithms (Logistic, Neural Net, Random Forest, and XGBoost) 
+                    to detect fraudulent credit card transactions. Some of the techniques used are
+                    Smoothing in Target Encoding and Feature selection (Filter: univariate model performance, Wrapper: forward stepwise selection.)'
+                    label='Supervised learning'
                     path='https://documentcloud.adobe.com/link/review?uri=urn:aaid:scds:US:5936684f-5f0e-46c2-913e-fca662a95d5f' />
 
                     <CardItem 
                     src='images/text.png'
-                    title='Fraudulent-News-Detection'
-                    text='Participated in the Codeday Hackathon 
-                    and won the "Best Mobile Application" award 
-                    out of 60 teams by creating a social motivation 
-                    Android Application. My team and I engineered a mobile 
-                    CRUD application using Java, Firebase, XML, and Agile.'
-                    label='Project'
+                    title='Fraudulent News Detection'
+                    text='Created an aiding tool in detecting fraudulent news
+                    that has been on the rise on social media and news platforms online.
+                    The main features that went into the models are the Title and Text of news articles,
+                    which were transformed into embedding vectors using Word2Vec embedding model.'
+                    label='Natural Language Processing'
                     path='https://github.com/Sayaka-K/Fraudulent-News-Detection/blob/main/Model%20Analysis%20to%20Provide%20Aid%20in%20Detecting%20Fake%20News.pdf' />
                 </ul>
             
